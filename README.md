e045b8941db06c7437d323e0b4e52a77e904c258
niamtokik <github@steepath.eu>
2023-08-09 19:21:23 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# berty
+
+A clean, safe and flexible implementation of BERT, a data-structure
+format inspired by Erlang ETF.
+
+
+## Build
+
+    $ rebar3 compile
+
+# FAQ
+
+## Why creating another BERT implementation?
+
+Mainly because of atoms management but also to expand BERT and
+BERT-RPC.
---
+++ src/berty_app.erl
@@ -0,0 +1,15 @@
+%%%-------------------------------------------------------------------
+%% @doc berty public API
+%% @end
+%%%-------------------------------------------------------------------
+-module(berty_app).
+-behaviour(application).
+-export([start/2, stop/1]).
+
+start(_StartType, _StartArgs) ->
+    berty_sup:start_link().
+
+stop(_State) ->
+    ok.
+
+%% internal functions
